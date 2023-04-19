961fa891a298e7d49528fec0f733f35633ed7798
whiskey-2 <43121415+whiskey-2@users.noreply.github.com>
2023-04-19 02:44:57 +0000

    added attack and fleeing mechanism directly from tutorial (to be modified to my tastes soon. The attack functions are too simple right now, dont like them)

--- main.py
+++ main.py
@@ -130,5 +130,71 @@ async def hunt(ctx):
   await ctx.message.reply(
     f"You encounter a {enemy.name}. Do you `!fight` or `!flee`?")
 
+@bot.command(name="fight", help="Fight the current enemy.")
+async def fight(ctx):
+    character = load_character(ctx.message.author.id)
+
+    if character.mode != GameMode.BATTLE:
+        await ctx.message.reply("Can only call this command in battle!")
+        return
+
+    # Simulate battle
+    enemy = character.battling
+
+    # Character attacks
+    damage, killed = character.fight(enemy)
+    if damage:
+        await ctx.message.reply(f"{character.name} attacks {enemy.name}, dealing {damage} damage!")
+    else:
+        await ctx.message.reply(f"{character.name} swings at {enemy.name}, but misses!")
+
+    # End battle in victory if enemy killed
+    if killed:
+      xp, gold, ready_to_level_up = character.defeat(enemy)
+
+      await ctx.message.reply(f"{character.name} vanquished the {enemy.name}, earning {xp} XP and {gold} GOLD. HP: {character.hp}/{character.max_hp}.")
+
+      if ready_to_level_up:
+        await ctx.message.reply(f"{character.name} has earned enough XP to advance to level {character.level+1}. Enter `!levelup` with the stat (HP, ATTACK, DEFENSE) you would like to increase. e.g. `!levelup hp` or `!levelup attack`.")
+
+      return
+
+    # Enemy attacks
+    damage, killed = enemy.fight(character)
+    if damage:
+        await ctx.message.reply(f"{enemy.name} attacks {character.name}, dealing {damage} damage!")
+    else:
+        await ctx.message.reply(f"{enemy.name} tries to attack {character.name}, but misses!")
+
+    character.save_to_db() #enemy.fight() does not save automatically
+
+    # End battle in death if character killed
+    if killed:
+        character.die()
+
+        await ctx.message.reply(f"{character.name} was defeated by a {enemy.name} and is no more. Rest in peace, brave adventurer.")
+        return
+
+    # No deaths, battle continues
+    await ctx.message.reply(f"The battle rages on! Do you `!fight` or `!flee`?")
+
+@bot.command(name="flee", help="Flee the current enemy.")
+async def flee(ctx):
+    character = load_character(ctx.message.author.id)
+
+    if character.mode != GameMode.BATTLE:
+        await ctx.message.reply("Can only call this command in battle!")
+        return
+
+    enemy = character.battling
+    damage, killed = character.flee(enemy)
+
+    if killed:
+        character.die()
+        await ctx.message.reply(f"{character.name} was killed fleeing the {enemy.name}, and is no more. Rest in peace, brave adventurer.")
+    elif damage:
+        await ctx.message.reply(f"{character.name} flees the {enemy.name}, taking {damage} damage. HP: {character.hp}/{character.max_hp}")
+    else:
+        await ctx.message.reply(f"{character.name} flees the {enemy.name} with their life but not their dignity intact. HP: {character.hp}/{character.max_hp}")
 
 bot.run(DISCORD_TOKEN)
