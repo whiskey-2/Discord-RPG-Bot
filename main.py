4b362a7c9a92fec1af8a7d94314deb1ec9ff0569
whiskey-2 <43121415+whiskey-2@users.noreply.github.com>
2023-04-19 02:39:51 +0000

    status command now displays stamina

--- main.py
+++ main.py
@@ -35,21 +35,20 @@ def status_embed(ctx, character):
                         description=mode_text,
                         color=MODE_COLOR[character.mode])
   embed.set_author(name=ctx.author.display_name,
-                   icon_url=ctx.author.avatar_url)
+                   icon_url=ctx.author.avatar)
 
   # Stats field
   _, xp_needed = character.ready_to_level_up()
 
-  embed.add_field(name="Stats",
-                  value=f"""
+  embed.add_field(name="Stats", value=f"""
 **HP:**    {character.hp}/{character.max_hp}
 **ATTACK:**   {character.attack}
 **DEFENSE:**   {character.defense}
 **MANA:**  {character.mana}
+**STAMINA** {character.stamina}
 **LEVEL:** {character.level}
 **XP:**    {character.xp}/{character.xp+xp_needed}
-    """,
-                  inline=True)
+    """, inline=True)
 
   # Inventory field
   inventory_text = f"Gold: {character.gold}\n"
